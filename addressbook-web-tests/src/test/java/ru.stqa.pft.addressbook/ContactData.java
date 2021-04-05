6220ea91f44d87d411a4497a89807dcbb2471b87
ebolotov <caterinabolotova@gmail.com>
2021-04-05 12:21:56 +0200

    Add class ContactData

---
+++ addressbook-web-tests/src/test/java/ru.stqa.pft.addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private String firstname;
+    private String middlename;
+    private String lastname;
+    private String nickname;
+    private String title;
+
+    public ContactData(String firstname, String middlename, String lastname, String nickname, String title) {
+        this.firstname = firstname;
+        this.middlename = middlename;
+        this.lastname = lastname;
+        this.nickname = nickname;
+        this.title = title;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
--- addressbook-web-tests/src/test/java/ru.stqa.pft.addressbook/NewContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru.stqa.pft.addressbook/NewContactCreationTests.java
@@ -20,13 +20,11 @@ public class NewContactCreationTests {
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
-
-
   @Test
   public void testNewContactCreationTests() throws Exception {
     login();
     goToContactPage("add new");
-    fillContacForm();
+    fillContactForm(new ContactData("Ivan", "Peter", "Ivanov", "iv", "ivTitle"));
     submitContactCreation("(//input[@name='submit'])[2]");
     goToContactPage("home");
     goToContactPage("Logout");
@@ -36,31 +34,23 @@ public class NewContactCreationTests {
     driver.findElement(By.xpath(s)).click();
   }
 
-  private void fillContacForm() throws InterruptedException {
+  private void fillContactForm(ContactData contactData) throws InterruptedException {
     driver.findElement(By.name("firstname")).click();
     driver.findElement(By.name("firstname")).clear();
-    driver.findElement(By.name("firstname")).sendKeys("test");
+    driver.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
     driver.findElement(By.name("middlename")).click();
     driver.findElement(By.name("middlename")).clear();
-    driver.findElement(By.name("middlename")).sendKeys("test2");
+    driver.findElement(By.name("middlename")).sendKeys(contactData.getMiddlename());
     driver.findElement(By.name("lastname")).click();
     driver.findElement(By.name("lastname")).clear();
-    driver.findElement(By.name("lastname")).sendKeys("test3");
+    driver.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
     driver.findElement(By.name("nickname")).click();
     driver.findElement(By.name("nickname")).clear();
-    driver.findElement(By.name("nickname")).sendKeys("test4");
+    driver.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
     driver.findElement(By.name("title")).click();
     driver.findElement(By.name("title")).clear();
-    driver.findElement(By.name("title")).sendKeys("test5");
+    driver.findElement(By.name("title")).sendKeys(contactData.getTitle());
   }
-
-//  private void addPhoto () throws InterruptedException {
-//    driver.findElement(By.name("photo")).click();
-//    driver.findElement(By.xpath("/html/body/div/div[4]/form/input[7]")).click();
-//    Thread.sleep(2000);
-//    driver.findElement(By.name("photo")).clear();
-//    driver.findElement(By.name("photo")).sendKeys("C:\\fakepath\\image (20).png");
-//  }
 
   private void goToContactPage(String s) {
     driver.findElement(By.linkText(s)).click();
