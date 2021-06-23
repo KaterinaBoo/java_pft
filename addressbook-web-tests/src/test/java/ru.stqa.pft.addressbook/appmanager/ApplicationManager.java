6d8d88928d114c907ca74d11c912d09701b03443
ebolotov <caterinabolotova@gmail.com>
2021-06-23 15:15:54 +0200

    Executing tests in different browsers: Chrome, FF, IE + Add class Equality

--- addressbook-web-tests/src/test/java/ru.stqa.pft.addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru.stqa.pft.addressbook/appmanager/ApplicationManager.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
@@ -16,10 +19,24 @@ public class ApplicationManager {
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private ContactHelper contactHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
-        driver = new ChromeDriver();
+        if (browser.equals(BrowserType.FIREFOX)) {
+            driver = new FirefoxDriver();
+            System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\geckodriver.exe");
+        } else if (browser.equals(BrowserType.CHROME)) {
+            driver = new ChromeDriver();
+            System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+        } else if (browser.equals(BrowserType.IE)) {
+            driver = new InternetExplorerDriver();
+            System.setProperty("webdriver.ie.driver", "C:\\Selenium\\IEDriverServer.exe");
+        }
+
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get("http://localhost:8090/addressbook/group.php?new=New+group");
         contactHelper = new ContactHelper(driver);
